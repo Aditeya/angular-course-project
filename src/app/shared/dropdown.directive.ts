153b0472f9e0ef778d6c3c60b0daee3fca46dbc1
aditeya <22963960+Aditeya@users.noreply.github.com>
2022-04-29 12:43:13 +0530

    added dropdown deselect from anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -14,7 +14,9 @@ export class DropdownDirective {
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
 }
