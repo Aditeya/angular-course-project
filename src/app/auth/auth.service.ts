9aa1892314a04aadd36b06ecef674a827aa3f241
Aditeya <22963960+Aditeya@users.noreply.github.com>
2022-05-04 18:14:59 +0530

    video 299; added error handling w/ messages for login and sign up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,45 +1,68 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  url =
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDqlDNoPfoCjLF8MXtOO8VFe9zG-91o7hM';
+  private API_KEY = 'AIzaSyDqlDNoPfoCjLF8MXtOO8VFe9zG-91o7hM';
+  private SIGN_UP_URL =
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+    this.API_KEY;
+  private LOGIN_URL =
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+    this.API_KEY;
 
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
     return this.http
-      .post<AuthResponseData>(this.url, {
+      .post<AuthResponseData>(this.SIGN_UP_URL, {
         email: email,
         password: password,
         returnSecureToken: true,
       })
-      .pipe(
-        catchError((errorRes) => {
-          let errorMessage = 'An unknown auth error occured';
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
+      .pipe(catchError(this.handleError));
+  }
 
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email exists already';
-          }
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(this.LOGIN_URL, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      })
+      .pipe(catchError(this.handleError));
+  }
 
-          return throwError(errorMessage);
-        })
-      );
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown auth error occured';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is incorrect';
+        break;
+    }
+
+    return throwError(errorMessage);
   }
 }
