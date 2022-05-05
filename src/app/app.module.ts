365bd685cb782cec2a297f817342249855a128f4
aditeya <22963960+Aditeya@users.noreply.github.com>
2022-05-05 07:05:37 +0530

    video 333; added lazy loading to auth, recipes and shopping-list modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AuthModule } from './auth/auth.module';
 import { HeaderComponent } from './header/header.component';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    RecipesModule,
-    ShoppingListModule,
-    AuthModule,
-    SharedModule,
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
